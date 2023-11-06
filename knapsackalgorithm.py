87e5cf622a70f046a54e3e2778effc1a653bed30
mavssvighnesh <109013315+mavssvighnesh@users.noreply.github.com>
2023-11-06 18:32:29 +0530

    knapsackalgorithm 0/1

---
+++ knapsackalgorithm.py
@@ -0,0 +1,19 @@
+def knapsack_01(values, weights, capacity):
+    n = len(values)
+    dp = [[0] * (capacity + 1) for _ in range(n + 1)]
+    
+    for i in range(1, n + 1):
+        for w in range(capacity + 1):
+            if weights[i - 1] <= w:
+                dp[i][w] = max(dp[i - 1][w], dp[i - 1][w - weights[i - 1]] + values[i - 1])
+            else:
+                dp[i][w] = dp[i - 1][w]
+    
+    return dp[n][capacity]
+
+values = [10, 50, 60, 30]
+weights = [2, 5, 10, 5]
+capacity = 10
+
+max_value = knapsack_01(values, weights, capacity)
+print("Maximum value:", max_value)
